2001e9c644745a30b74bf074b5d7873cb83c1cbd
Firzen592798 <tyranitar_nb@hotmail.com>
2017-03-28 23:40:03 -0300

    componente pagina edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { LoginPage } from '../pages/login/login';
 
+
 import { AuthService } from '../providers/auth-service';
 
 import {
